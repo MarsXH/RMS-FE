a4e09ecc2acbd53328faab02df4209f83d9714e5
XH <95548854@qq.com>
2020-03-23 14:17:55 +0800

    [test] test router

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,5 +1,5 @@
 
-export default function ({ app, store, error, redirect, req }) {
+export default async function ({ app, store, error, redirect, req }) {
   if (req && (req.url === '/__webpack_hmr' || req.url === '/__webpack_hmr/client' || req.url === '/api/v1/validateToken')) return
   await store.dispatch('auth/validateToken')
   if (!store.state.auth.user.user_id) {
